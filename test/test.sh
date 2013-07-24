85153a1f3bdf99581ff6e363cdce4242724a97e4
Sebastian Menski <sebastian.menski@googlemail.com>
2013-07-25 01:41:29 +0200

    use mrk.sed executable

--- test/test.sh
+++ test/test.sh
@@ -4,19 +4,12 @@ if [ $(basename $(pwd)) != "test" ]; then
   cd test/
 fi
 
-if [ -e ../mrk.sed ]; then
-  mrk='sed -nf ../mrk.sed'
-else
-  echo "Unable to find mrk.sed" >&2
-  exit 1
-fi
-
 for txt in *.txt
 do
   filename=$(basename $txt)
   testname=${filename%.*}
   echo "Running test '$testname'"
-  $mrk $testname.txt | diff -y --suppress-common-lines - $testname.html
+  ../mrk.sed $testname.txt | diff -y --suppress-common-lines - $testname.html
   if [ $? -eq 0 ]; then
     echo " passed"
   fi
